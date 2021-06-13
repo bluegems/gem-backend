33969efc92d55ef1bb39d6640332aac905f3be01
parinithshekar <parinith.1114@gmail.com>
2021-06-13 19:06:25 +0530

    Remove schema and data scripts

--- src/test/java/com/bluegems/server/gembackend/GemBackendApplicationTests.java
+++ src/test/java/com/bluegems/server/gembackend/GemBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GemBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
